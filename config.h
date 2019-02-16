4afe63f23ac0f2fb8824d0b8ca70d78c1ddc0c17
andrei <andrei.i.ivanov@gmail.com>
2019-02-16 17:39:21 +0900

    push preparation

--- config.h
+++ config.h
@@ -13,11 +13,20 @@
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
+/* Define to 1 if you have the `crypto' library (-lcrypto). */
+#define HAVE_LIBCRYPTO 1
+
+/* Define to 1 if you have the `curl' library (-lcurl). */
+#define HAVE_LIBCURL 1
+
 /* Define to 1 if you have the `microhttpd' library (-lmicrohttpd). */
 #define HAVE_LIBMICROHTTPD 1
 
 /* Define to 1 if you have the `nanomsg' library (-lnanomsg). */
 #define HAVE_LIBNANOMSG 1
+
+/* Define to 1 if you have the `netsnmp' library (-lnetsnmp). */
+#define HAVE_LIBNETSNMP 1
 
 /* Define to 1 if you have the <limits.h> header file. */
 #define HAVE_LIMITS_H 1
